5aeea0ee3dc13c1a8f94af7c16a32a25d01d9692
Jissa Aan Juby <jissaannjuby@gmail.com>
2025-06-10 13:39:51 +0530

    Updated Day 3 and added Day 4 examples

--- day_3_python.py
+++ day_3_python.py
@@ -101,6 +101,98 @@ else:
         print(c, "is the largest")
 
 
-    
 
 
+#checking a if a num is positive , negative or zero
+a= int(input("enter your number"))
+if a!=0:
+    if a<0:
+        print("the number is negative")
+    else:
+        print("the number is positive")
+else:
+    print("thr number is zero")
+
+
+
+#checking if a letter is vowel or consonant
+a= input("enter your letter")
+if a=="A" or a=="a":
+    print("your letter is a vowel ")
+elif a=="E" or a== "e":
+    print("your letter is a vowel")
+elif a=="I" or a=="i":
+    print("your letter is a vowel")
+elif a=="o" or a=="O":
+    print("your letter is a vowel")
+elif a=="u" or "U":
+    print("your letter is a vowel")
+else:
+    print("your letter is a consonant")
+
+
+#program to give a discount for purchased amount
+#less than 5k then 30%
+# less than 3k then 20%
+# less than 1k then 10%
+# else no discounts
+
+a= int(input("enter your purchased amount"))
+if a<5000:
+    print("you have a discount of 30%")
+    c= a*30/100
+elif a<3000:
+    print("you have discount of 20%")
+    c= a*20/100
+elif a<1000:
+    print("you have a discount of 10%")
+    c=a*10/100
+else:
+    print("you have no discounts ")
+    c=0
+print("**********purchased details************")
+print("your purchased amount is", a)
+print("your saved",c)
+print("your payment is",a-c)
+
+
+#write  a program to menu driven option for a mathematical operation 
+
+a= int(input("enter your first number "))
+b= int(input("enter your second number "))
+c= int(input("1)sum\n2)difference\n3)product\n4)division\nenter your choice"))
+if c==1:
+    print("your sum is ",a+b)
+elif c==2:
+    print("your difference is ",a-b)
+elif c==3:
+    print("your product is", a*b)
+elif c==4:
+    print("your quotient is", a/b)
+else:
+    print("invalid choice")
+
+
+
+#program to check area if radius is greater than 20 else take perimeter perimeter 
+a= int(input("enter radius "))
+if a>20:
+    print("area is ",3.14*a*a)
+else:
+    print("perimeter is ",2*3.14*a)
+
+
+#example of identity operator 
+a=2
+b=3
+d=2
+print(a is b)
+print(a is not d)
+
+
+#example of membership operator
+a="jissa"
+if "j" in a:
+    print("j in jissa")
+else:
+    print("j is not in jissa")
---
+++ day_4python.py
@@ -0,0 +1,115 @@
+#learnt about for loops and the way its use
+print("jissa")
+
+for i in range(11):
+    print("jissa")
+
+
+for i in range (11):
+    print(i,"jissa")
+
+#start wuth index value 1
+for i in range(1,11):
+    print(i,"jissa")
+
+
+#odd index values
+for i in range (1,11,2):
+    print(i,"jissa")
+
+
+#even index values
+for i in range (2,11,2):
+    print(i,"jissa")
+
+
+#program to print the values upto n
+n=int(input("enter your input"))
+for i in range (1,n+1):
+    print(i)
+
+
+#print odd n values
+n= int(input("enter your input"))
+for i in range (1,n+1):
+    if i%2==1:
+        print(i)
+    
+
+
+
+#print n even values
+n= int(input ("enter your input"))
+for i in range (1,n+1):
+    if i%2==0:
+        print(i)
+
+
+#print odd numbers upto 20
+for i in range (20):
+    if i%2==1:
+        print(i)
+
+
+#print squares of numbers from 1 to 10
+for i in range (1,11):
+    print(i*i)
+
+
+#program to write fibbonacci series
+#0,1,1,2,3,5.....233
+a=0
+b=1
+print(a)
+print(b)
+#for c in range (233): (cant use this coz it will get repeated 233 times)
+c=0
+while c<233:
+    c=a+b
+    print(c)
+    a=b
+    b=c
+
+
+#program to print values from 1 to 10
+i=1
+while i<11:
+    print(i)
+    i=i+1
+
+
+#program to print n odd numbers
+n = int(input("enter your input"))
+i=0
+while i<n+1:
+    if i%2==1:
+        print(i)
+    i=i+1
+
+#program odd nos till 0 to 20
+i=0
+while i<20:
+    if i%2==0:
+        print(i)
+    i= i+1
+
+
+
+#find the sum of n natural numbers
+n=int(input("enter your input"))
+i=1
+s=0
+while i<n+1:
+    s=s+i
+    i=i+1
+print("sum of ",n, "natural numbers is",s)
+
+#using for loop
+n=int(input("enter your input"))
+s=0
+for i in range (1,n+1):
+    s=s+1
+print(s)
+
+#program to find the factorial of a number
+n=int(input("enter your"))
